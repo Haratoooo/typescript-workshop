7113b484092fcca33478edb6c13b82211779d474
Heart Alvern Sumicad <145334591+Haratoooo@users.noreply.github.com>
2024-09-01 21:23:00 +0800

    Delete build/src directory

--- build/src/map.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const sqaureNumber = (numbers) => {
-    const squaredNumbers = numbers.map(number => number * number);
-    return squaredNumbers;
-};
-//# sourceMappingURL=map.js.map
